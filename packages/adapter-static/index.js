8967932af4f2c97503cdfd777f369319e537eb33
Sascha Tandel <514405+sastan@users.noreply.github.com>
2021-12-20 18:10:06 +0100

    feat: add precompress option to adapter-static

--- packages/adapter-static/index.js
+++ packages/adapter-static/index.js
@@ -1,5 +1,17 @@
+import {
+	createReadStream,
+	createWriteStream,
+	existsSync,
+} from 'fs';
+import { pipeline } from 'stream';
+import glob from 'tiny-glob';
+import { promisify } from 'util';
+import zlib from 'zlib';
+
+const pipe = promisify(pipeline);
+
 /** @type {import('.')} */
-export default function ({ pages = 'build', assets = pages, fallback } = {}) {
+export default function ({ pages = 'build', assets = pages, fallback, precompress } = {}) {
 	return {
 		name: '@sveltejs/adapter-static',
 
@@ -10,11 +22,59 @@ export default function ({ pages = 'build', assets = pages, fallback } = {}) {
 			utils.copy_static_files(assets);
 			utils.copy_client_files(assets);
 
+			if (precompress) {
+				utils.log.minor('Compressing assets');
+				await compress(assets);
+			}
+
 			await utils.prerender({
 				fallback,
 				all: !fallback,
 				dest: pages
 			});
+
+			if (precompress && existsSync(`${pages}/prerendered`)) {
+				utils.log.minor('Compressing prerendered pages');
+				await compress(`${pages}/prerendered`);
+			}
 		}
 	};
+}
+
+/**
+ * @param {string} directory
+ */
+ async function compress(directory) {
+	const files = await glob('**/*.{html,js,json,css,svg,xml}', {
+		cwd: directory,
+		dot: true,
+		absolute: true,
+		filesOnly: true
+	});
+
+	await Promise.all(
+		files.map((file) => Promise.all([compress_file(file, 'gz'), compress_file(file, 'br')]))
+	);
+}
+
+/**
+ * @param {string} file
+ * @param {'gz' | 'br'} format
+ */
+async function compress_file(file, format = 'gz') {
+	const compress =
+		format == 'br'
+			? zlib.createBrotliCompress({
+					params: {
+						[zlib.constants.BROTLI_PARAM_MODE]: zlib.constants.BROTLI_MODE_TEXT,
+						[zlib.constants.BROTLI_PARAM_QUALITY]: zlib.constants.BROTLI_MAX_QUALITY,
+						[zlib.constants.BROTLI_PARAM_SIZE_HINT]: statSync(file).size
+					}
+			  })
+			: zlib.createGzip({ level: zlib.constants.Z_BEST_COMPRESSION });
+
+	const source = createReadStream(file);
+	const destination = createWriteStream(`${file}.${format}`);
+
+	await pipe(source, compress, destination);
 }
